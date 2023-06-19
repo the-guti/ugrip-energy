cdb5bd909691e6624552683ff1c9193aa0908add
shhd77 <shahad.alshehyari@mbzuai.ac.ae>
2023-06-19 15:25:15 +0400

    added a2c

---
+++ train_a2c.py
@@ -0,0 +1,75 @@
+import time
+import os
+import argparse
+from utils import set_seed, make_env
+from stable_baselines3 import A2C
+import wandb
+from wandb.integration.sb3 import WandbCallback
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env_path", type=str, default="configs/env.yaml")
+    parser.add_argument("--logs_path", type=str, default="runs/a2c/")
+    parser.add_argument("--algorithm", type=str, default="a2c")
+    parser.add_argument("--total_timesteps", type=int, default=8000000)
+    parser.add_argument("--seed", type=int, default=1337)
+    parser.add_argument("--learning_rate", type=float, default=0.0009)
+   # parser.add_argument("--n_steps", type=int, default=20)
+    parser.add_argument("--gamma", type=float, default=0.988)
+    parser.add_argument("--vf_coef", type=float, default=0.85)
+    parser.add_argument("--qf_nns", type=int, default=256)
+    parser.add_argument("--pi_nns", type=int, default=256)
+    args = parser.parse_args()
+
+    # Set seed for reproducibility
+    algorithm_config = {
+        "learning_rate": args.learning_rate,
+       # "batch_size": args.batch_size,
+        "gamma": args.gamma,
+        "vf_coef": args.vf_coef,
+        "policy_kwargs": {
+            "net_arch": {
+                "pi": [args.pi_nns, 2 * args.pi_nns],
+                "qf": [args.qf_nns, 2 * args.qf_nns],
+            }
+        },
+        "verbose": 1,
+    }
+
+    # Initialize the environment
+    env, env_config = make_env(args.env_path, "train.csv") 
+
+    # Initialize WandB
+    with wandb.init(
+        project="ugrip-energy",
+        config={
+            "algorithm": args.algorithm,
+            "total_timesteps": args.total_timesteps,
+            "seed": args.seed,
+            "algorithm_config": algorithm_config,
+            "environment_config": env_config,
+        },
+        name=args.algorithm + "_" + str(int(time.time())), 
+        sync_tensorboard=True,
+    ) as run:
+
+        # Set up logging directory
+        logs_path = args.logs_path + run.id + "/"
+        os.makedirs(logs_path, exist_ok=True)
+
+        # Initialize the model
+        model = A2C("MlpPolicy", env, **algorithm_config, tensorboard_log=logs_path)
+
+        # Train the model
+        model.learn(
+            total_timesteps=args.total_timesteps,
+            callback=WandbCallback()
+        )
+
+        # Save the environment and the model
+        model.save(logs_path + "model")
+        env.save(logs_path + "env.pkl")
+
+if __name__ == "__main__":
+    main()
